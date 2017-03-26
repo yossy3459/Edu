073c0213e2d2a64738b7adcb686ef91c01173259
yossy3459 <vagrant@localhost.localdomain>
2017-03-27 02:12:51 +0900

    first commit

---
+++ 170301.rb
@@ -0,0 +1,7 @@
+colors = ["red","blue","yellow"]
+
+p colors[0]
+p colors[-1]
+p colors[0..2]
+p colors[0...2]
+p colors[5]
---
+++ hello.rb
@@ -0,0 +1,32 @@
+=begin
+msg = "hello world"
+puts msg
+
+
+msg = "hello world again"
+puts msg
+
+VERSION = 1.1
+puts VERSION
+
+VERSION = 1.2
+puts VERSION
+
+=end
+
+
+# p 4.8.class
+# p 4.8.methods
+#
+# p 10 + 3
+# p 10 * 3
+# p 2.4 * 2
+# p 10 / 3
+# p 10 % 3
+# p 10.0 / 3
+# p Rational(2,5) + Rational(3,4)
+# p 2/5r + 3/4r
+
+p 52.6.round
+p 52.6.floor
+p 52.6.ceil
